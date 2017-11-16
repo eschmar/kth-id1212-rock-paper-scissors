f9ac81b6aaf304d78cf3f920ac922309db5a0aff
Marcel Eschmann <marcel.e@nectima.se>
2017-11-16 21:52:30 +0100

    Update game view

--- src/io/eschmann/controller/GameController.java
+++ src/io/eschmann/controller/GameController.java
@@ -1,4 +1,44 @@
 package io.eschmann.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 public class GameController {
+    @FXML
+    public Label usernameLabel;
+
+    @FXML
+    public GridPane mainGrid;
+
+    @FXML
+    public TextField myIpText;
+
+    @FXML
+    public Label scoreWinLabel;
+
+    @FXML
+    public Label scoreLossLabel;
+
+    @FXML
+    public Label roundNumberLabel;
+
+    @FXML
+    public Label playerCountLabel;
+
+    @FXML
+    public Button rockBtn;
+
+    @FXML
+    public Button paperBtn;
+
+    @FXML
+    public Button scissorsBtn;
+
+    @FXML
+    public TextArea logTextarea;
+
 }
--- src/io/eschmann/model/Player.java
+++ src/io/eschmann/model/Player.java
@@ -2,15 +2,19 @@ package io.eschmann.model;
 
 import io.eschmann.net.server.ReceiverServer;
 
+import java.util.ArrayList;
+
 public class Player {
     public String ip;
     public ReceiverServer server;
     public String username;
 
+    public ArrayList<String> opponents;
+
     public Player(String ip) {
         this.ip = ip;
         initServer();
-
+        this.opponents = new ArrayList<String>();
         System.out.println("New player on " + this.ip + ":" + this.server.port + "!");
     }
 
